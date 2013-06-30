ccdd9ef2dd7a45b8edd3248ae3e8b0d15085875d
Gilberto Conde <gilbertorconde@gmail.com>
2013-07-01 00:43:20 +0100

    DB and README.md updated

--- README.md
+++ README.md
@@ -1,15 +1,57 @@
-# Português:
+# Português
 
-Esta aplicação foi feita com o proposito de aprofundar os meus conhecimentos em php. Trata-se de um blog engine simples. Todo o codigo usado neste projecto (à excepção do tinymce) está abrangida pela licença GPLv3 da GNU.
+## Sobre
+
+Esta aplicação foi feita com o proposito de aprofundar os meus
+conhecimentos em php. Trata-se de um blog engine simples.  
+Todo o codigo usado neste projecto (à excepção do tinymce) está
+abrangida pela licença GPLv3 da GNU.
 
 O tinyMCE está abrangido pela licensa LGPLv2 da GNU.
 
-projecto GIT do tinyMCE: [https://github.com/tinymce/tinymce](https://github.com/tinymce/tinymce)
+projecto GIT do tinyMCE:  
+[https://github.com/tinymce/tinymce](https://github.com/tinymce/tinymce)
 
-# English:
+## Instruções
 
-This application was made with the purpose of deepening my knowledge in php. This is a simple blog engine. All the code used in this project (except tinymce) is covered by the GNU GPLv3 license.
+É necessário criar uma base de dados mysql seguindo a estrutura do
+ficheiro phpBlogDB.sql
+
+É também necessário trocar o nome do ficheiro core/init.inc.php.example
+para core/init.inc.php, e prencher as variaveis do mesmo com
+os valores correspondentes ao acésso à base de dados.  
+Caso seja usada a estrutura do ficheiro phpBlogDB.sql, as credenciais
+para entrar no blog são:
+
+* Nome: test_user
+* Email: test@example.com
+* Senha: 6ZaxN2Vzm9NUJT2y
+
+# English
+
+## About
+
+This application was made with the purpose of deepening my knowledge
+in php. This is a simple blog engine.  
+All the code used in this project (except tinymce) is covered by the
+GNU GPLv3 license.
 
 The tinyMCE is covered by the GNU LGPLv2 license.
 
-tiniMCE GIT project: [https://github.com/tinymce/tinymce](https://github.com/tinymce/tinymce)
+tiniMCE GIT project:  
+[https://github.com/tinymce/tinymce](https://github.com/tinymce/tinymce)
+
+## Istructions
+
+Is necessary to create a database mysql following the structure of
+file phpBlogDB.sql
+
+It is also necessary to change the name of the file
+core/init.inc.php.example for core/init.inc.php, and fill the
+variables with the values ​​corresponding to the access database.  
+If you use phpBlogDB.sql as structure file, the credential
+to enter the blog are:
+
+* User: test_user
+* Email: test@example.com
+* Password: 6ZaxN2Vzm9NUJT2y
---
+++ phpBlogDB.sql
@@ -0,0 +1,98 @@
+-- phpMyAdmin SQL Dump
+-- version 3.4.11.1deb1
+-- http://www.phpmyadmin.net
+--
+-- Máquina: localhost
+-- Data de Criação: 01-Jul-2013 às 00:20
+-- Versão do servidor: 5.5.31
+-- versão do PHP: 5.4.16
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Base de Dados: `phpBlog`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `comments`
+--
+
+CREATE TABLE IF NOT EXISTS `comments` (
+  `comment_id` int(6) NOT NULL AUTO_INCREMENT,
+  `post_id` int(6) NOT NULL,
+  `comment_body` text COLLATE utf8_bin NOT NULL,
+  `comment_user` varchar(55) COLLATE utf8_bin NOT NULL,
+  `comment_date` datetime NOT NULL,
+  PRIMARY KEY (`comment_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=9 ;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `login_attempts`
+--
+
+CREATE TABLE IF NOT EXISTS `login_attempts` (
+  `user_id` int(11) NOT NULL,
+  `time` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `members`
+--
+
+CREATE TABLE IF NOT EXISTS `members` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(30) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `about` text NOT NULL,
+  `password` char(128) NOT NULL,
+  `salt` char(128) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3 ;
+
+--
+-- Extraindo dados da tabela `members`
+--
+
+INSERT INTO `members` (`id`, `username`, `email`, `about`, `password`, `salt`) VALUES
+(2, 'test_user', 'test@example.com', 'POR ESCREVER', '00807432eae173f652f2064bdca1b61b290b52d40e429a7d295d76a71084aa96c0233b82f1feac45529e0726559645acaed6f3ae58a286b9f075916ebf66cacc', 'f9aab579fc1b41ed0c44fe4ecdbfcdb4cb99b9023abb241a6db833288f4eea3c02f76e0d35204a8695077dcf81932aa59006423976224be0390395bae152d4ef');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `posts`
+--
+
+CREATE TABLE IF NOT EXISTS `posts` (
+  `post_id` int(6) NOT NULL AUTO_INCREMENT,
+  `post_title` varchar(255) COLLATE utf8_bin NOT NULL,
+  `post_body` text COLLATE utf8_bin NOT NULL,
+  `post_user` varchar(55) COLLATE utf8_bin NOT NULL,
+  `post_date` datetime NOT NULL,
+  PRIMARY KEY (`post_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=13 ;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `tags`
+--
+
+CREATE TABLE IF NOT EXISTS `tags` (
+  `tag_id` int(11) NOT NULL AUTO_INCREMENT,
+  `post_id` int(11) NOT NULL,
+  `tag_name` varchar(128) NOT NULL,
+  PRIMARY KEY (`tag_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=26 ;
